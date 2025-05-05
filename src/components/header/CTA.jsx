780adf699d02c6aad1eb2a76b3bb8390b2740782
Dhrupal Patel <106724754+Dhrupalpatelpatel@users.noreply.github.com>
2025-05-05 19:25:07 +0530

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import CV from  '../../assets/cv1.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-      <a href={CV} download className='btn'>Download CV</a>
+      <a href={CV} download className='btn'>Downloadss CV</a>
       <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
